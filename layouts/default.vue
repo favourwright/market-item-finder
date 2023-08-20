26702f43f6f56527bb99cc6c6cd5c66e621aa8e7
Favour <gismozfexie@yahoo.com>
2023-08-20 14:24:05 +0100

    chore: make auth states dynamic

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,6 @@
     <header
       class="tw-w-full tw-bg-black tw-text-white">
       <div class="tw-px-6 sm:tw-px-10 tw-p-4 tw-max-w-7xl tw-mx-auto tw-flex tw-justify-between tw-items-center">
-        <!-- logo -->
         <div class="tw-flex tw-items-center tw-gap-10">
           <span class="tw-select-none tw-relative">
             <span class="tw-tracking-tighter tw-text-2xl">
@@ -11,24 +10,41 @@
             </span>
             <NuxtLink to="/" class="tw-absolute tw-inset-0"></NuxtLink>
           </span>
-          <nav>
+          <nav v-if="isSeller">
             <NuxtLink to="/requests">Active Requests</NuxtLink>
           </nav>
         </div>
   
         <div class="tw-space-x-2">
-          <NuxtLink
-            to="/login"
-            class="tw-inline-block tw-p-1 tw-select-none">
-            Login
-          </NuxtLink>
-          <NuxtLink
-            to="/register"
-            class="tw-inline-block tw-p-1 tw-px-3 tw-rounded-full tw-bg-white
-            tw-select-none tw-text-black hover:tw-bg-white/80
-            tw-transition-all tw-duration-300">
-            Signup
-          </NuxtLink>
+          <template v-if="!isAnonymous && isSignedIn">
+            <button
+              class="tw-bg-black tw-text-white tw-inline-block tw-rounded-md tw-px-2 tw-p-1 tw-select-none"
+              @click="logout">
+              Logout
+            </button>
+            <NuxtLink
+              :to="`/accounts/${user?.uid}`"
+              class="tw-inline-block tw-p-1 tw-px-3 tw-rounded-full tw-bg-white
+              tw-select-none tw-text-black hover:tw-bg-white/80
+              tw-transition-all tw-duration-300">
+              My Account
+            </NuxtLink>
+          </template>
+
+          <template v-else>
+            <NuxtLink
+              to="/login"
+              class="tw-inline-block tw-p-1 tw-select-none">
+              Login
+            </NuxtLink>
+            <NuxtLink
+              to="/register"
+              class="tw-inline-block tw-p-1 tw-px-3 tw-rounded-full tw-bg-white
+              tw-select-none tw-text-black hover:tw-bg-white/80
+              tw-transition-all tw-duration-300">
+              Signup
+            </NuxtLink>
+          </template>
         </div>
       </div>
     </header>
@@ -64,31 +80,32 @@ import {
   signInAnonymously,
   signOut,
 } from 'firebase/auth'
-// import { User, AccountType } from '@/types'
+import { User, AccountType } from '@/types'
 
 const auth = useFirebaseAuth() // only exists on client side
 const user = useCurrentUser()
 // onMounted(async ()=>{
-//   const user = await signInAnonymously(auth!)
+//   signOut(auth!)
+//   // const user = await signInAnonymously(auth!)
 // })
 
 const env = useRuntimeConfig().public
 
 const isAnonymous  = computed(() => user.value?.isAnonymous)
 const isSignedIn = computed(() => user.value !== null)
-// const userCookie = useCookie<User>('user')
-// const isFarmer = computed(() => userCookie.value?.accountType === AccountType.FARMER)
-// const isBuyer = computed(() => userCookie.value?.accountType === AccountType.BUYER)
-// const router = useRouter()
-// const route = useRoute()
-// const logout = async () => {
-//   await signOut(auth!).then(() => {
-//     userCookie.value = null as unknown as User
-//   })
+const userCookie = useCookie<User>('user')
+const isSeller = computed(() => userCookie.value?.accountType === AccountType.SELLER)
+const isBuyer = computed(() => userCookie.value?.accountType === AccountType.BUYER)
+const router = useRouter()
+const route = useRoute()
+const logout = async () => {
+  await signOut(auth!).then(() => {
+    userCookie.value = null as unknown as User
+  })
 
-//   // only redirect if user is on a protected route
-//   if (route.meta.requiresAuth) {
-//     router.push('/login')
-//   }
-// }
+  // only redirect if user is on a protected route
+  if (route.meta.requiresAuth) {
+    router.push('/login')
+  }
+}
 </script>
