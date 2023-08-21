3be9a2432cd31899b3897be194268ad47aa16c11
Favour <gismozfexie@yahoo.com>
2023-08-21 18:46:47 +0100

    chore: add middleware & make image previewable

--- pages/requests/[[id]].vue
+++ pages/requests/[[id]].vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <div class="tw-h-[70vh] tw-bg-gray-100">
+    <div class="tw-h-[70vh] tw-bg-gray-100 tw-relative">
       <v-col class="tw-h-full pa-0">
         <ClientOnly>
           <v-carousel v-model="carousel" cycle :show-arrows="(requestDetails?.images || []).length>1"
             hide-delimiter-background hide-delimiters height="100%">
             <v-carousel-item ripple v-for="(image, n) in requestDetails?.images" :key="n" :src="image" cover>
+              <div class="tw-absolute tw-inset-x-0 tw-bottom-0 tw-max-w-7xl tw-mx-auto">
+                <div class="tw-p-6 sm:tw-p-10 tw-flex tw-justify-end">
+                  <button
+                    @click="previewImage(image)"
+                    class="tw-bg-black tw-text-white tw-p-1 tw-ring-1 tw-ring-white/30">
+                    <v-icon>mdi-arrow-expand-all</v-icon>
+                  </button>
+                </div>
+              </div>
             </v-carousel-item>
           </v-carousel>
         </ClientOnly>
@@ -17,12 +26,25 @@
     <div v-else class="tw-text-center tw-py-20">
       <p class="tw-text-4xl tw-text-black">loading...</p>
     </div>
+
+    <v-dialog v-model="imagePreview" max-width="500">
+      <div class="tw-bg-white tw-rounded-xl">
+        <img
+          :src="previewedImage"
+          class="tw-h-full tw-w-full tw-object-contain">
+      </div>
+    </v-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
 import { getDatabase, onValue, ref as RTDBRef } from 'firebase/database';
 import { Request } from '@/types';
+
+definePageMeta({
+  middleware: 'auth',
+  requiresAuth: true,
+})
 
 const route = useRoute()
 const router = useRouter()
@@ -44,4 +66,11 @@ const fetchUserRequests = async () => {
   });
 }
 onMounted(fetchUserRequests)
+
+const imagePreview = ref(false)
+const previewedImage = ref('')
+const previewImage = (src: string) => {
+  imagePreview.value = true
+  previewedImage.value = src
+}
 </script>
